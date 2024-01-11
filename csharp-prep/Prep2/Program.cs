61e88d42ccde978efafec990d8e55c0b7fa35e4a
Ethanlc99 <ethanlcarlson99@gmail.com>
2024-01-10 19:18:25 -0700

    Complete prep assignement per instructions

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade percentage");
+        string response = Console.ReadLine();
+
+        int grade = int.Parse(response);
+
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >=70)
+        {
+            letter = "C";
+        }
+        else if (grade >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade for the class was {letter}.");
+
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.WriteLine("Congratulations on passing the class!!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time chump");
+        }
+
     }
 }
