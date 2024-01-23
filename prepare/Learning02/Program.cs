aea23a38851838319dfd5e33c5038f9bbde4f0a5
Ethanlc99 <ethanlcarlson99@gmail.com>
2024-01-23 14:50:30 -0700

    Completed "Abstraction Learning" project using these files.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Algorithm's Intern";
+        job1._company = "IMSAR";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Automotive Engineer";
+        job2._company = "Tesla";
+        job2._startYear = 2010;
+        job2._endYear = 2015;
+
+        Resume resume1 = new Resume();
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1._name = "Joe Biden";
+
+        resume1.Display();
+
+
+        // job1.Display();
+        // job2.Display();
     }
 }
