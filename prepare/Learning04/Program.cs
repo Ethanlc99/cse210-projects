d1f7bbbf7f0713e5250db2ea02cdb1f28cbb349e
Ethanlc99 <118488172+Ethanlc99@users.noreply.github.com>
2024-02-14 06:29:54 +0000

    Completed Inheritance prepare per instructions

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Jonny", "Coding C#");
+        Console.WriteLine(assignment1.GetSummary());
+
+        Console.WriteLine();
+
+        WritingAssignment assignment2 = new WritingAssignment("Billy", "English 301", "Meditations");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingInformation());
+
+        Console.WriteLine();
+
+        MathAssignment assignment3 = new MathAssignment("Trash", "Math 101", "Section 1", "5.6-6.9");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetHomeworkList());
+
     }
 }
